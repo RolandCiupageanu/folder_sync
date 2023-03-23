0fe6b4157df7b446973b09afcb9ffb8f934aa9ee
Ciupageanu Roland <ciupageanu_r@yahoo.com>
2023-03-23 21:04:26 +0200

    First commit

---
+++ folder_sync.py
@@ -0,0 +1,295 @@
+# Roland Ciupageanu 2023
+# A unidirectional folder syncronisation
+
+import os
+import sys
+import shutil
+import datetime
+import time
+
+# ==========================Helper functions===================================
+def create_log(path):
+    """This function will create the log.txt file in the specified path
+    and write to it the first line of info, returns False if the log.txt couldn't be created"""
+    # Time in dd/mm/YY H:M:S format, string
+    time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    try:
+        with open(path + "\\" + "log.txt", "w") as logfile:
+            logfile.write(f"{time} Created log.txt in path: {path}\n")
+    except:
+        # If False = the user will be infromed only in the console 
+        return False
+
+def get_time(path):
+    """This function will return the time of the last modification of a file
+    in the format ex: 2023-03-20 22:03:52.461867 """
+    time = os.path.getmtime(path)
+    formated_time = datetime.datetime.fromtimestamp(time)
+    return formated_time
+
+def current_time():
+    """Returns current time in dd/mm/YY H:M:S format as a string"""
+    current_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    return current_time
+
+def get_path():
+    """This function will return the source path,replica path and log file path
+    provided from the command line arguments"""
+    # Paths in cmd must be provided using "\\", ex: C:\\Users\\Roland\\Desktop\\folder0
+    try:
+        source = sys.argv[1]
+        replica = sys.argv[2]
+        log = sys.argv[3]
+
+    except IndexError:
+        sys.exit("To few command line arguments!")
+
+    if len(sys.argv) > 5:
+        sys.exit("To many command line arguments!")
+    return source, replica, log
+
+def get_file(path):
+    """This function will return a list containg the files/folders of the path.
+    False if exception is raised and the path could not be accesed"""
+    try:
+        list_file = sorted (os.listdir(path))
+    except FileNotFoundError:
+        return False
+    return list_file
+
+def copy_file(files_source, files_replica, path_source,path_replica):
+    """This function will copy any files or folders from source to replica 
+    if they are present in the source folder and not in the replica folder.
+    Returns a list of the copied files"""
+    copied_files = []
+    for file in files_source:
+        if file not in files_replica:
+            # If the target is a file with extension
+            if "." in file:
+                shutil.copy(path_source + "\\" + file, path_replica + "\\" + file)
+                copied_files.append(file)
+            # If the target is a folder
+            else:
+                shutil.copytree(path_source + "\\" + file, path_replica + "\\" + file)
+                copied_files.append(file)
+    return copied_files
+   
+def replace_same(files_source, files_replica, path_source,path_replica):
+    """This function will replace files/folders from replica with the ones from the source if:
+        the files have the same names and they have different modification dates.
+        Returns a list of replaced files in the replica folder"""
+    replaced_files = []
+    for n in range(len(files_source)):
+            source_file_path = path_source + "\\" + files_source[n]
+            replica_file_path = path_replica + "\\" + files_replica[n]
+            
+            # If the files/ folders from source/replica have different modification dates
+            if get_time(source_file_path) != get_time(replica_file_path):
+                # If the target is a file
+                if os.path.isfile(replica_file_path):
+                    os.remove(replica_file_path)
+                    shutil.copy(source_file_path,replica_file_path)
+                    replaced_files.append(files_source[n])
+
+                # If the target is a folder
+                elif os.path.isdir(replica_file_path): 
+                    shutil.rmtree(replica_file_path)
+                    shutil.copytree(source_file_path,replica_file_path)
+                    replaced_files.append(files_source[n])
+    return replaced_files
+                    
+def delete_file(files_source, files_replica, path_source,path_replica):
+    """This function will delete files/ folders from replica if they are not present
+    in the source. Returns a list of the deleted files"""
+    deleted_files = []
+    for file in files_replica:
+        if file not in files_source:
+            replica_file_path = path_replica + "\\" + file
+            # If the target is a file
+            if os.path.isfile(replica_file_path):
+                os.remove(replica_file_path)
+                deleted_files.append(file)
+                
+            # If the target is a folder
+            elif os.path.isdir(replica_file_path): 
+                shutil.rmtree(replica_file_path)
+                deleted_files.append(file)
+    return deleted_files
+
+# ============================Main function==============================================
+def main():
+    # Getting the pats of the source, replica and log folder
+    path = get_path()
+
+    # Defining source,replica and log file folder paths
+    source_path = path[0]
+    replica_path = path[1]
+
+    log_file_status = 0
+
+    log_path = path[2]
+
+    if get_file(log_path) == False:
+        # Signals that the log file couldn't be created.
+        # The program will continue but without any log file, 
+        # messages will be printed only in the console
+
+        # Time in dd/mm/YY H:M:S format, string
+        time = current_time()
+        log_file_status = 1
+        print(f"{time} The log.txt file could'n be created at path: {log_path}")
+
+    if log_file_status == 0:
+        # Log file creation
+        # If the log file does not already at the log_path, create the log.txt file
+        if "log.txt" not in get_file(log_path):
+            # Time in dd/mm/YY H:M:S format, string
+            time = current_time()
+            
+            create_log(log_path)
+            if create_log(log_path) != False:
+                # Variable to signal the creation of the log_file_status (0=ok, 1=NOK)
+                log_file_status = 0
+                print(f"{time} Created log.txt in path: {log_path}")
+                
+            else:
+                # Signals that the log file couldn't be created.
+                # The program will continue but without any log file, 
+                # messages will be printed only in the console
+                log_file_status = 1
+                print(f"{time} The log.txt file could'n be created at path: {log_path}")
+        else:
+            log_file_status = 0
+
+    # Getting a list of files in both source and replica folder
+    if get_file(source_path) == False:
+        sys.exit(f"The source path {source_path} is not a valid path!")
+    list_source = get_file(source_path)
+
+    if get_file(replica_path) == False:
+        sys.exit(f"The replica path {replica_path} is not a valid path!")
+    list_replica = get_file(replica_path)
+
+    # What the program does in the different situations:
+
+    # Case 1
+    # Both source and replica folders are empty ==> there is nothing to do
+    if len(list_source) == len(list_replica) == 0:
+        # Time in dd/mm/YY H:M:S format, string
+        time = current_time()
+        
+        # Log file and message handling
+        # If the log.txt is present in the log_path(no error and the file was created)
+        if log_file_status == 0:
+            with open(log_path + "\\" + "log.txt", "a") as logfile:
+                logfile.write(f"{time} Source and replica folders are empty, folders are in sync!\n")
+            print(f"{time} Source and replica folders are empty, folders are in sync!")
+        else:
+            # No log file created, messages display only in the console
+            print(f"{time} Source and replica folders are empty, folders are in sync!NO log file created!")
+
+    # Case2
+    # More files/ foders in the replica then in the source ==> delete the nonsync files from replica folder
+    elif len(list_source) < len(list_replica):
+        # Time in dd/mm/YY H:M:S format, string
+        time = current_time()
+
+        deleted_list = delete_file(list_source,list_replica,source_path,replica_path)
+
+        delete_file(list_source,list_replica,source_path,replica_path)
+
+        # Log file and message handling
+        # If the log.txt is present in the log_path(no error and the file was created)
+        if log_file_status == 0:
+            with open(log_path + "\\" + "log.txt", "a") as logfile:
+                logfile.write(f"{time} The files {deleted_list} were deleted from the replica folder\n")
+            print(f"{time} The files {deleted_list} were deleted from the replica folder")
+        else:
+            # No log file created, messages display only in the console
+            print(f"{time} The files {deleted_list} were deleted from the replica folder! NO log file created!")
+
+    # Case 3
+    # More files/ foders in the source then in the replica ==> copy files from source to replica folder
+    elif len(list_source) > len(list_replica):
+        # Time in dd/mm/YY H:M:S format, string
+        time = current_time()
+
+        copy_list = copy_file(list_source,list_replica,source_path,replica_path)
+
+        # Copy the files/ folders from source folder to replica folder if they are not present
+        copy_file(list_source,list_replica,source_path,replica_path)
+
+        # Log file and message handling
+        # If the log.txt is present in the log_path(no error and the file was created)
+        if log_file_status == 0:
+            with open(log_path + "\\" + "log.txt", "a") as logfile:
+                logfile.write(f"{time} The files {copy_list} were copied from source to replica folder\n")
+            print(f"{time} The files {copy_list} were copied from source to replica folder")
+        else:
+            # No log file created, messages display only in the console
+            print(f"{time} The files {copy_list} were copied from source to replica folder! NO log file created!")
+
+    # Case 4
+    # Same file/ folder names in both source and replica ==> if modification dates in source/ replica differ
+    # Replace files/ folders from replica with the ones from source
+    elif list_source == list_replica:
+        # Time in dd/mm/YY H:M:S format, string
+        time = current_time()
+
+        replace_list = replace_same(list_source,list_replica,source_path,replica_path)
+
+        replace_same(list_source,list_replica,source_path,replica_path)
+        
+        # Log file and message handling
+        # If the log.txt is present in the log_path(no error and the file was created)
+        if log_file_status == 0:
+            with open(log_path + "\\" + "log.txt", "a") as logfile:
+                logfile.write(f"{time} The files {replace_list} were replaced in the replica folder\n")
+            print(f"{time} The files {replace_list} were replaced in the replica folder")
+        else:
+            # No log file created, messages display only in the console
+            print(f"{time} The files {replace_list} were replaced in the replica folder! NO log file created!")
+
+    # The final part that shows that the two folders are in sync
+    # Time in dd/mm/YY H:M:S format, string
+    time = current_time()
+
+    # Log file and message handling
+    # If the log.txt is present in the log_path(no error and the file was created)
+    if log_file_status == 0:
+        with open(log_path + "\\" + "log.txt", "a") as logfile:
+            logfile.write(f"{time} The source and replica folders are in sync!\n")
+        print(f"{time} The source and replica folders are in sync!")
+    else:
+        # No log file created, messages display only in the console
+        print(f"{time} The source and replica folders are in sync!! NO log file created!")
+
+    print("=====EXECUTION COMPLETED=====")
+
+if __name__ == "__main__":
+    # If until here not enough command line arguments were supplied (>5 needed)
+    if len(sys.argv) > 5:
+        sys.exit("To many command linde arguments!")
+    try:
+        # Converting the scheduled_time argument to int, and then to minutes
+        # Accepts even a float scheduled_time, it will be rounded to the nearest int
+        scheduled_time = int(sys.argv[4])*60
+        
+    # If until here not enough command line arguments were supplied (<5 needed)
+    except IndexError:
+        sys.exit("To few command linde arguments!")
+        
+    # If ValueError program exits with the following message
+    except ValueError:
+        sys.exit("Not a valid time argument, please supply int or float argument!")
+        
+    # If scheduled_time is set to 0, the program will execute only one time
+    if scheduled_time == 0:
+        main()
+        
+    else:
+        # Program executes every (scheduled_time*60) + execution time (seconds)
+        while True:
+            main()
+            time.sleep(scheduled_time)
+
---
+++ readme.md
@@ -0,0 +1,83 @@
+# Project name: folder sync
+## A program used to to synchronize two folders in unidirectional mode
+
+
+### 1. Project description:
+
+As automation becomes more and more used and needed, one may need to link two folders containing different files or folders.
+
+The two folders are named intuitive:
+- source folder : the folder with that the replica folder will be synced
+- replica folder: the folder that is synced with the source folder
+
+Other therms:
+- source path : the path of the source folder
+- replica path : the path of the replica folder
+- log : a log.txt file in which all operations are tracked
+- log path : the path of the log file
+- scheduled time : the time interval at which the program is executed again
+
+These two folders are synchronized only one way: the files and folders from the replica folder are synchronized with the files and folders from the source folder. 
+
+### 2. Logic and usage:
+The program comunicates with the user via command line arguments.
+The user must must provide the following command line argumets format:
+
+The interpreter: example: python 
+
+sys.argv[0]: example: folder_sync.py
+
+Path of the source folder:
+sys.argv[1]: example: C:\\Users\\Roland\\Desktop\\folder0
+
+Path of the replica folder:
+sys.argv[2]: example: C:\\Users\\Roland\\Desktop\\folder1
+
+Path of the log folder:
+sys.argv[3]: example: C:\\Users\\Roland\\Desktop\\log
+
+The time interval at which the program is executed again:
+If this argument is 0, the program will execute once.
+If the argument is a non zero integer, for example 3, it will be converted into minutes, so the program will run forever every 3 minutes.
+If the argument couldn't be converted to an int, the program will exit with a message.
+sys.argv[4]: example: 0
+
+Example of the full path:
+**python folder_sync.py C:\\Users\\Roland\\Desktop\\folder0 C:\\Users\\Roland\\Desktop\\folder1 C:\\Users\\Roland\\Desktop\\log 0**
+
+where:
+
+python : is the name of the interpreter
+folder_sync.py : the name of the file to execute
+C:\\Users\\Roland\\Desktop\\folder0 : path of the source folder
+C:\\Users\\Roland\\Desktop\\folder1 : path of the replica folder
+C:\\Users\\Roland\\Desktop\\log : path of the log.txt file
+0  : the scheduled time (with 0 the program will only execute once)
+
+
+
+
+### Program logic:
+1. Arguments 1, 2, 3 and 4 are converted to paths.
+If there are more then 5 arguments in total or the first three couldn't be converted to paths, the program will exit with a message.
+
+2. The log file log.txt will be created at the log path (arg[3]), if the log file can not be accesed the program will continue whithout a log file, and the messages will be displayed only in the console, else each operation will be appended to the log file and also printed to the console.
+
+3. A list of the files and folders in both source and replica files will be created. If the paths of these files can not be accesed, the program will exit with a message.
+
+4. Next comes the file manipulation in the different situations.
+Case 1: Both source and replica folders are empty, so there is nothing to do because both folders are in sync. The message will be appended to the log file(if the file can be created) and printed to the console.
+
+5. Case 2: More files/ foders in the replica then in the source, so we need to delete the nonsync files from replica folder. The files will be deleted, and added to a list. The list will appear in the message and it will appended in the log file(if the file can be created) and printed in the console.
+
+6. Case 3: More files/ foders in the source then in the replica  folder, so we need to copy files from source to replica folder. The files will be copied, and added to a list. The list will appear in the message and it will appended in the log file(if the file can be created) and printed in the console.
+
+7. Case 4: Same file/ folder names in both source and replica folder, if modification dates in source/ replica differ, replace files/ folders from replica with the ones from source. The files will be replaced, and added to a list. The list will appear in the message and it will appended in the log file(if the file can be created) and printed in the console.
+
+8. The final step, where the program will show if the two folders are in sync. The message will be appended in the log file(if the file can be created) and it will be printed in the console.
+
+9. Scheduled time: using this time provided in sys.argv[4], the program will repeat periodically (see the above section: "logic and usage")
+
+### 3. Requirements
+No additional libraries need to be installed.
+I've used only Python3 standard libraries, so there is no need to pip install any libraries.
